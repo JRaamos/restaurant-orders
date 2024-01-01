c9729bae9b4ceb9720bf211b9bf3edce84403d0a
JRaamos <jhonyramos46@gmail.com>
2024-01-01 14:01:22 -0300

    feat: 03 - Mapeamento Pratos <> Ingredientes

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,31 @@
-# Req 3
+import csv
+from typing import Dict
+
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self._process_data(source_path)
+
+    def _process_data(self, source_path: str) -> None:
+        dish_table: Dict[str, Dish] = {}
+
+        with open(source_path) as csv_file:
+            data = csv.DictReader(csv_file)
+
+            for row in data:
+                dish_name, price, ingredient_name, recipe_amount = (
+                    row["dish"],
+                    float(row["price"]),
+                    row["ingredient"],
+                    int(row["recipe_amount"]),
+                )
+
+                dish = dish_table.setdefault(dish_name, Dish(dish_name, price))
+                ingredient = Ingredient(ingredient_name)
+                dish.add_ingredient_dependency(ingredient, recipe_amount)
+
+                self.dishes.add(dish)
