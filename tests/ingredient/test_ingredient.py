ea4fb33775fd986710bce644d33934b20aae03a2
JRaamos <jhonyramos46@gmail.com>
2024-01-01 13:06:59 -0300

    feat: 01 - Testando classes já implementadas parte 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,31 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
-# Req 1
 def test_ingredient():
-    pass
+    def create_ingredient(name):
+        return Ingredient(name)
+
+    ingredient = create_ingredient("queijo mussarela")
+    assert isinstance(ingredient, Ingredient)
+    assert ingredient.name == "queijo mussarela"
+
+    expected_restrictions = {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
+    assert ingredient.restrictions == expected_restrictions
+
+    expected_repr = f"Ingredient('{ingredient.name}')"
+    assert repr(ingredient) == expected_repr
+
+    ingredient1 = create_ingredient("queijo mussarela")
+    ingredient2 = create_ingredient("queijo mussarela")
+    ingredient3 = create_ingredient("bacon")
+    assert ingredient1 == ingredient2
+    assert not (ingredient1 == ingredient3)
+
+    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) != hash(ingredient3)
+
+    assert ingredient.name == "queijo mussarela"
+    assert ingredient.restrictions == expected_restrictions
+
+    unexpected_restrictions = {Restriction.GLUTEN, Restriction.SEAFOOD}
+    assert ingredient.restrictions != unexpected_restrictions
